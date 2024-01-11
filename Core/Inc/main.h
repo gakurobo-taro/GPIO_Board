22c283311d954693b4116e35852499e46c939be4
sabanekko3 <tetayaae@gmail.com>
2024-01-11 13:28:32 +0900

    first

---
+++ Core/Inc/main.h
@@ -0,0 +1,97 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f3xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define GPIO9_Pin GPIO_PIN_1
+#define GPIO9_GPIO_Port GPIOA
+#define GPIO8_Pin GPIO_PIN_2
+#define GPIO8_GPIO_Port GPIOA
+#define GPIO7_Pin GPIO_PIN_3
+#define GPIO7_GPIO_Port GPIOA
+#define GPIO6_Pin GPIO_PIN_4
+#define GPIO6_GPIO_Port GPIOA
+#define GPIO5_Pin GPIO_PIN_5
+#define GPIO5_GPIO_Port GPIOA
+#define GPIO4_Pin GPIO_PIN_6
+#define GPIO4_GPIO_Port GPIOA
+#define GPIO3_Pin GPIO_PIN_7
+#define GPIO3_GPIO_Port GPIOA
+#define GPIO2_Pin GPIO_PIN_0
+#define GPIO2_GPIO_Port GPIOB
+#define GPIO1_Pin GPIO_PIN_1
+#define GPIO1_GPIO_Port GPIOB
+#define ID0_Pin GPIO_PIN_4
+#define ID0_GPIO_Port GPIOB
+#define ID2_Pin GPIO_PIN_5
+#define ID2_GPIO_Port GPIOB
+#define ID1_Pin GPIO_PIN_6
+#define ID1_GPIO_Port GPIOB
+#define ID3_Pin GPIO_PIN_7
+#define ID3_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
